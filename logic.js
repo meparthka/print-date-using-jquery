d95e062e12b64693839c30d9d0c498f0d48c4892
elparthka <parthka.2005@gmail.com>
2022-04-11 22:40:36 +0530

    123

--- logic.js
+++ logic.js
@@ -1,9 +1,21 @@
-var getBtn = $("#get_btn")
+var getBtn1 = $("#get_btn_1")
+var getBtn2 = $("#get_btn_2")
+var getBtn3 = $("#get_btn_3")
 var dateText = $("#date_text")
 var removeBtn = $("#remove_btn")
 
-getBtn.on('click', () => {
+getBtn1.on('click', () => {
     dateText.html(new Date())
+})
+
+getBtn2.on('click', () => {
+    let d = new Date();
+    dateText.html((d.getMonth() + 1) + "/" + d.getDate() + "/" + d.getFullYear())
+})
+
+getBtn3.on('click', () => {
+    let d = new Date();
+    dateText.html(d.getDate() + "/" + (d.getMonth() + 1) + "/" + d.getFullYear())
 })
 
 removeBtn.on('click', () => {
